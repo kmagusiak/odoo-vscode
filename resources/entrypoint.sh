02c1e65cd31681c1a1f2baa0f6fdb88f580a25e4
Krzysztof Magusiak <chris.magusiak@gmail.com>
2022-03-19 22:46:59 +0000

    Move addons and enterprise path

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -17,6 +17,7 @@ fi
 # set all variables
 : ${ODOO_EXTRA_ADDONS:=/mnt/extra-addons}
 EXTRA_ADDONS_PATHS=$(python3 getaddons.py ${ODOO_EXTRA_ADDONS} 2>&1)
+[ ! -d "${ODOO_ENTERPRISE}" ] || EXTRA_ADDONS_PATHS+=",${ODOO_ENTERPRISE}"
 
 if [ ! -f ${ODOO_RC} ]
 then
